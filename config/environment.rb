2d05e5d788822202bd46d4849ab6395760cb8881
eschechter <schechter.evan@gmail.com>
2019-06-14 11:45:47 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
-require 'bundler/setup'
+require "bundler/setup"
 Bundler.require
 
-require 'active_record'
-require 'rake'
+require "active_record"
+require "rake"
 
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each { |f| require f }
+Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each { |f| require f }
 
 DBRegistry[ENV["PLAYLISTER_ENV"]].connect!
 DB = ActiveRecord::Base.connection
@@ -13,3 +13,4 @@ DB = ActiveRecord::Base.connection
 if ENV["PLAYLISTER_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
 end
+ActiveRecord::Base.logger.level = 1
